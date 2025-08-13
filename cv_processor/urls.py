3d0fa3bea9e2be27ef646e820994880cddc386a9
kunteshbastola <163876327+kunteshbastola@users.noreply.github.com>
2025-08-13 18:04:42 +0545

    auth

--- cv_processor/urls.py
+++ cv_processor/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('analyzer.urls')),
+     # Allauth URLs
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
