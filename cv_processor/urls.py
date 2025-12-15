9eb4614f955f7b195eec332b9d72f41c47baab1f
kunteshbastola <kunteshbastola5@gmail.com>
2025-12-15 11:38:00 +0545

    final commit

--- cv_processor/urls.py
+++ cv_processor/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('analyzer.urls')),
-     # Allauth URLs
+    # Allauth URLs
     path('accounts/', include('allauth.urls')),
 ]
 
